b6d7b84751e08542dbf53f4744daadfe4a2b09eb
XiaoChenYung <18730222889@126.com>
2015-11-04 18:45:20 +0800

    初始化

---
+++ BaiduMapTest/BaiduMapTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiduMapTest
+//
+//  Created by 杨晓晨 on 15/11/4.
+//  Copyright © 2015年 杨晓晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
